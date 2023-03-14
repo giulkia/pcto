573f5244f1e777084ef023931cbaf53711357bf1
giulkia <127745779+giulkia@users.noreply.github.com>
2023-03-14 10:18:57 +0100

    Add files via upload

---
+++ ciao.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int eta;
+    char sesso;
+printf("qual è la tua età? qual è il tuo sesso? ");
+scanf("%d %c", &eta, &sesso);
+printf("la mia età è %d\n, il mio sesso è %c\n",eta, sesso);
+
+}
