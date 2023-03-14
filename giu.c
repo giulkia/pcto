8ca746e31542718de3588e50095ae1f1a269eb95
giulkia <127745779+giulkia@users.noreply.github.com>
2023-03-14 11:58:28 +0100

    Add files via upload

---
+++ giu.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    char sesso;
+    printf("qual è il tuo sesso?");
+    scanf(" %c", &sesso);
+
+    if(sesso=='m')
+    {
+        printf("sei maschio\n");
+    }
+    else if(sesso=='f')
+{
+    printf("sei una femmina\n");
+}
+else if ( sesso=='n')
+{
+    printf("sei non identificato\n");
+}
+else
+{
+    printf("errore\n");
+}
+
+ }
